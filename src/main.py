f55bceeef3f07a9a22bf14fd01933cda17200104
Murilo Reis <muriloreisriosalves@gmail.com>
2020-04-07 14:38:29 -0300

    Geração de graficos, tabelas excel, limpeza e junção de dados

--- src/main.py
+++ src/main.py
@@ -1,9 +1,67 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import stats.utils as util
 
 def calc_percentage(df, column, decimals=2):
     """ Returns a series with the percentages of a column"""
     return (df[column].value_counts(normalize=True)).round(decimals).rename('Porcentagem')
 
-print('Hello World')
+def clean_file(f):
+    df = pd.read_excel(f)
+    df.columns = df.columns.str.strip()
+    for col in df.columns:
+        if isinstance(col, str):
+            df[col] = df[col].str.strip()
+    return df
+
+def load_ordens(df):
+    ordens = ['ACCIPITRIFORMES', 'CATHARTIFORMES', 'STRIGIFORMES', 'FALCONIFORMES']
+    lst = []
+    for ordem in ordens:
+        ord = clean_file('src/files/'+ordem+'.xlsx')
+        for nome in ord[ordem].unique():
+            lst.append((nome, ordem))
+        especies = pd.DataFrame(lst).set_index(0)[1].rename('ORDEM')
+    df = df.merge(especies, left_on='ESPÉCIE', right_index=True).reset_index(drop=True)
+    return df
+
+def pieplot(df, path):
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.pie(df)
+    plt.legend(labels=df.index, bbox_to_anchor=(0.9, 0.9), loc="best")
+    plt.tight_layout()
+    plt.savefig(path)
+
+df = clean_file('src/files/rapinantes_anual.xlsx')
+df['DATA'].replace({'jan':'Jan'}, inplace=True)
+meses = df['DATA'].unique()
+df = load_ordens(df)
+porcentagem_animais = df["ESPÉCIE"].value_counts(normalize=True).round(2)
+porcentagem_motivo_entrada = calc_percentage(df.groupby(['ESPÉCIE']), 'MOTIVO DE ENTRADA')
+df_plot = porcentagem_motivo_entrada.reset_index()
+porcentagem_motivo_entrada = porcentagem_motivo_entrada.unstack(level=-1, fill_value=0)
+g = sns.catplot(kind='bar', x='MOTIVO DE ENTRADA', y='Porcentagem', data=df_plot, col='ESPÉCIE', col_wrap=6, legend=True)
+g.set_xticklabels(rotation=90)
+plt.tight_layout()
+plt.savefig('output/porcentagem_motivo_entrada.png')
+porcentagem_motivo_entrada['Total'] = porcentagem_animais
+porcentagem_motivo_entrada.to_excel('output/porcentagem_motivo_entrada.xlsx')
+
+porcentagem_traumas = df['MOTIVO DE ENTRADA'].value_counts(normalize=True).round(4)
+pieplot(porcentagem_traumas, 'output/porcentagem_traumas.png')
+porcentagem_traumas.to_excel('output/porcentagem_traumas.xlsx')
+
+porcentagem_ordem_mes = calc_percentage(df.groupby('DATA'), 'ORDEM').reindex(meses, level=0)
+df_plot = porcentagem_ordem_mes.reset_index()
+porcentagem_ordem_mes = porcentagem_ordem_mes.unstack(level=-1, fill_value=0)
+g = sns.catplot(kind='bar', x='ORDEM', y='Porcentagem', data=df_plot, col='DATA', col_wrap=6, legend=False)
+g.set_xticklabels(rotation=90)
+plt.tight_layout()
+plt.savefig('output/porcentagem_ordem_mes.png')
+porcentagem_ordem_mes.to_excel('output/porcentagem_ordem_mes.xlsx')
+
+porcentagem_ordem_anual = df['ORDEM'].value_counts(normalize=True)
+pieplot(porcentagem_ordem_anual, 'output/porcentagem_ordem_anual.png')
+porcentagem_ordem_anual.to_excel('output/porcentagem_ordem_anual.xlsx')
---
+++ src/stats/utils.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
+
+    return x, y
+
+def calc_bins(n_defaults):
+    "Calculating bins to plot the binomial PMF"
+    bins = np.arange(0, max(n_defaults) + 1.5) - 0.5
+    return bins
+
+def bootstrap_replicate_1d(data, func):
+    """Generate bootstrap replicate of 1D data."""
+    bs_sample = np.random.choice(data, len(data))
+    return func(bs_sample)
+
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+    # Initialize array of replicates
+    bs_replicates = np.empty(size)
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data, func)
+    return bs_replicates 
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
+
+    return bs_slope_reps, bs_intercept_reps
+
+def permutation_sample(data1, data2):
+    """Generate a permutation sample from two data sets."""
+
+    # Concatenate the data sets: data
+    data = np.concatenate((data1, data2))
+
+    # Permute the concatenated array: permuted_data
+    permuted_data = np.random.permutation(data)
+
+    # Split the permuted array into two: perm_sample_1, perm_sample_2
+    perm_sample_1 = permuted_data[:len(data1)]
+    perm_sample_2 = permuted_data[len(data1):]
+
+    return perm_sample_1, perm_sample_2
+
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+
+    return diff
+
+def shift_data_mean(data, mean):
+    return data - np.mean(data) + mean
+
+def confidence_interval(data, percentage):
+    x = 100 - percentage
+    return np.percentile(data, [x/2, 100-(x/2)])     
+
+def compute_percentiles(percentage):
+    x = 100 - percentage
+    return [x/2, 100-(x/2)]
